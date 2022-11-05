cffb3600c54478d9db143511a507d2eb8a91df95
Aleksandr Sokolov <bigpewm@gmail.com>
2022-11-05 23:55:53 +0300

    feat: runner sync server

---
+++ django_run_server_sync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+touch /tmp/app-initialized
+
+if [ "$DEBUG" = "1" ]; then \
+    python3 manage.py runserver "0.0.0.0:${BACKEND_PORT:-8000}"
+else \
+  daphne -b 0.0.0.0 -p "${BACKEND_PORT:-8000}" django_app.wsgi:application
+fi
