30b53c5bb4651562f659cda5b1f24ade4a592aed
Aleksandr Sokolov <52139992+bigpe@users.noreply.github.com>
2023-10-21 13:24:03 +0300

    Update django_run_server_sync.sh

--- django_run_server_sync.sh
+++ django_run_server_sync.sh
@@ -5,5 +5,5 @@ touch /tmp/app-initialized
 if [ "$DEBUG" = "1" ]; then \
     python3 manage.py runserver "0.0.0.0:${BACKEND_PORT:-8000}"
 else \
-  daphne -b 0.0.0.0 -p "${BACKEND_PORT:-8000}" django_app.wsgi:application
+  gunicorn -b 0.0.0.0 -p "${BACKEND_PORT:-8000}" django_app.wsgi:application
 fi
